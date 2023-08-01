c4a2cb9b2219d2ce44089295e57224bcc80d156a
AyshaUrmi <ayshaismail021@gmail.com>
2023-08-01 23:03:06 +0600

    second commit

--- js/todo.js
+++ js/todo.js
@@ -1,38 +1,32 @@
 
-let count = 0;
-document.getElementById("input-btn").addEventListener("click", function () {
-  
-  count += 1;
-  const myInputValue = document.getElementById("input-value").value;
- 
+        let count = 0;
+        document.getElementById("input-btn").addEventListener("click", function () {
+            count += 1;
+            const myInputValue = document.getElementById("input-value").value;
+            const parent = document.getElementById("content-container");
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
+                <td>${count}</td>
+                <td>${myInputValue}</td>
+                <td>
+                    <button class='btn btn-outline-info deletebtn'>Delete</button>
+                    <button class='btn btn-outline-warning'>Done</button>
+                </td>
+            `;
+            parent.appendChild(tr);
+            document.getElementById("input-value").value = "";
 
-  const parent = document.getElementById("content-container");
+            const elements = document.querySelectorAll(".deletebtn");
+            for (const value of elements) {
+                value.addEventListener("click", function (e) {
+                    e.target.parentNode.parentNode.style.display = "none";
+                });
+            }
+        });
 
-  const tr = document.createElement("tr");
-  tr.innerHTML = `
-    <td>${count}</td>
-    <td>${myInputValue}</td>
-    <td>
-    <button class='btn btn-outline-info>Delete</button>
-    <button class='btn btn-outline-warning'>Done</button>
-    </td>
-
-  
-  `;
-  parent.appendChild(tr);
-  document.getElementById("input-value").value = "";
-
-  const elements = document.querySelectorAll(".deletebtn");
-
-  for (const value of elements) {
-    value.addEventListener("click", function (e) {
-      // console.log(event.target.parentNode.parentNode);
-      e.target.parentNode.parentNode.style.display = "none";
-    });
-  }
-});
-
-document.getElementById("clear-btn").addEventListener("click", function (e) {
-  console.log(e.target.parentNode);
-  e.target.parentNode.style.display = "none";
-});
+        document.getElementById("clear-btn").addEventListener("click", function (e) {
+            const parent = document.getElementById("content-container");
+            parent.innerHTML = "";
+            
+        });
+   
